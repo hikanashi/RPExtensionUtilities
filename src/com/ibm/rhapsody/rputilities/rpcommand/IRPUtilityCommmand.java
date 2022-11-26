fbc8de4d86510c85f14ad6ebba0357aa415e40fe
hikanashi <hikanashi@gmail.com>
2022-11-26 18:33:49 +0900

    Count actions per swimlane #28

--- src/com/ibm/rhapsody/rputilities/rpcommand/IRPUtilityCommmand.java
+++ src/com/ibm/rhapsody/rputilities/rpcommand/IRPUtilityCommmand.java
@@ -3,7 +3,7 @@ package com.ibm.rhapsody.rputilities.rpcommand;
 import com.ibm.rhapsody.rputilities.rpcore.ARPObject;
 import com.telelogic.rhapsody.core.IRPModelElement;
 
-abstract class IRPUtilityCommmand extends ARPObject {
+public abstract class IRPUtilityCommmand extends ARPObject {
     
     protected IRPModelElement   m_element = null;
 
@@ -21,7 +21,7 @@ abstract class IRPUtilityCommmand extends ARPObject {
      * @param argment 右クリック時に選択されたメニュー(hepファイルに記載されたnameをデリミタで分割した配列)
      * @return コマンド実行結果(true:成功 false:失敗)
      */
-    abstract boolean command(String[] argment);
+    protected abstract boolean command(String[] argment);
  
     /**
      * 右クリック時に選択された要素を取得する
---
+++ src/com/ibm/rhapsody/rputilities/rpcommand/activitycount/ActiounCounter.java
@@ -0,0 +1,88 @@
+package com.ibm.rhapsody.rputilities.rpcommand.activitycount;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.ibm.rhapsody.rputilities.rpcore.ARPObject;
+
+public class ActiounCounter extends ARPObject {
+    final protected String NONE_NAME = "-None-";
+    final protected String FLOWINAL_NAME = "-flowfinal-";
+    final protected String ACTIVITYINAL_NAME = "-activityfinal-"; 
+
+    protected String packageName_ = "";
+    protected String activityName_ = "";
+    protected Map<String,Integer> count_action_ = new LinkedHashMap<String,Integer>();
+    protected Integer count_flowfinal_ = 0;
+    protected Integer count_activityfinal_ = 0;
+
+
+    public ActiounCounter(String packagename, String activityName) 
+    {
+        super(ActiounCounter.class);
+        packageName_ = packagename;
+        activityName_ = activityName;
+    }
+
+    public String getPackageName() {
+        return packageName_;
+    }
+
+    public String getActivityName() {
+        return activityName_;
+    }
+
+    public Map<String,Integer> getMap() {
+        Map<String,Integer> counter = new LinkedHashMap<String,Integer>(count_action_);
+        counter.put(FLOWINAL_NAME,count_flowfinal_);
+        counter.put(ACTIVITYINAL_NAME,count_activityfinal_);
+        return counter;
+    }
+
+    public void InitAction(String swimlane) {
+        CountAction(swimlane,0);
+    }
+
+    public void CountAction(String swimlane) {
+        CountAction(swimlane,1);
+    }
+
+    public void CountFlowFinal(String swimlane) {
+        count_flowfinal_++;
+    }
+
+    public void CountActivityFinal(String swimlane) {
+        count_activityfinal_++;
+    }
+
+    public void merge(ActiounCounter source) {
+        if(source == null) {
+            return;
+        }
+
+        count_flowfinal_ += source.count_flowfinal_;
+        count_activityfinal_ += source.count_activityfinal_;
+
+        source.count_action_.forEach((key, value) -> {
+            CountAction(key,value);
+        });        
+    }
+    
+    protected void CountAction(String swimlane,Integer increase) {
+        trace("Swimlane:"+ swimlane + " count:"+ increase);
+        
+        String register_name;
+        if(swimlane != null && swimlane.length() > 0) {
+            register_name = swimlane;
+        }
+        else {
+            register_name = NONE_NAME;
+        }
+
+        Integer count = count_action_.getOrDefault(register_name,0);
+        count += increase;
+        count_action_.put(register_name,count);
+    }
+
+
+
+}
